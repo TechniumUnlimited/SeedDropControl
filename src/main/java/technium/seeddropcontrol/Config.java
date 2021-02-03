bd2a5c0eb3c72350202a952e2135dca52f002f09
Technium <techniumunlimited@gmail.com>
2021-02-03 11:42:10 -0800

    Begin implementation of dead bush modifications

--- src/main/java/technium/seeddropcontrol/Config.java
+++ src/main/java/technium/seeddropcontrol/Config.java
@@ -12,6 +12,7 @@ public class Config {
     public static final ForgeConfigSpec CONFIG;
 
     public static ForgeConfigSpec.IntValue grass_wheat_chance;
+    public static ForgeConfigSpec.IntValue dead_bush_chance;
     public static ForgeConfigSpec.IntValue grass_mod_seed_chance;
     public static ForgeConfigSpec.BooleanValue planttech2_guidebook_drop;
     public static ForgeConfigSpec.BooleanValue debug_enabled;
@@ -19,6 +20,10 @@ public class Config {
     static {
         BUILDER.push("Vanilla Seed Options");
         vanilla(BUILDER);
+        BUILDER.pop();
+
+        BUILDER.push("Dead Bush Drops");
+        vanillaDeadBush(BUILDER);
         BUILDER.pop();
 
         BUILDER.push("Mod Compat Options");
@@ -33,10 +38,13 @@ public class Config {
     }
 
     public static void vanilla(ForgeConfigSpec.Builder config) {
-        grass_wheat_chance = config.comment("Increase or Decrease chance of drops of wheat seeds from grass, 100 is default, <100 is the x% of default (1 is 1% of default rate)").defineInRange("Drop Rate Change", 100, 1, 100);
+        grass_wheat_chance = config.comment("Decrease chance of drops of wheat seeds from grass, 100 is vanilla default drop rate, <100 is the x% of default (1 is 1% of default rate)").defineInRange("Drop Rate Change", 100, 1, 100);
+    }
+    public static void vanillaDeadBush(ForgeConfigSpec.Builder config) {
+        dead_bush_chance = config.comment("CURRENTLY UNUSED: Decrease chance of drops from all dead bush blocks, 100 is vanilla default drop rate, <100 is the x% of default (1 is 1% of default rate)").defineInRange("Drop Rate Change", 100, 1, 100);
     }
     public static void modcompat(ForgeConfigSpec.Builder config) {
-        grass_mod_seed_chance = config.comment("Increase or Decrease chance of drops of modded seeds from grass, 100 is default, <100 is the x% of default (1 is 1% of default rate)").defineInRange("Drop Rate Change", 100, 1, 100);
+        grass_mod_seed_chance = config.comment("Decrease chance of drops of modded seeds from grass, 100 is vanilla default drop rate, <100 is the x% of default (1 is 1% of default rate)").defineInRange("Drop Rate Change", 100, 1, 100);
         planttech2_guidebook_drop = config.comment("Mod Compat - Planttech2: Allow Planttech2's guidebook to drop?").define("True/False", true);
     }
     public static void debug(ForgeConfigSpec.Builder config) {
